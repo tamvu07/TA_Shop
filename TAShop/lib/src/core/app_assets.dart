32d636ae37c3c8a968c277f427a00d659d7dde9e
Tam Vu <tam_vu@onskyhealth.com>
2024-08-09 11:27:23 +0700

    *Update code

--- TAShop/lib/src/core/app_assets.dart
+++ TAShop/lib/src/core/app_assets.dart
@@ -1,4 +1,6 @@
 class AppAssets {
   static const String icAddPersons = 'assets/ic_add_persons.png';
   static const String icAdd = 'assets/ic_add.png';
+  static const String icEditUser = 'assets/ic_edit_user.png';
+  static const String icNextItem = 'assets/ic_next_item.png';
 }
--- TAShop/lib/src/screens/add_users_page.dart
+++ TAShop/lib/src/screens/add_users_page.dart
@@ -17,6 +17,7 @@ class _AddUsersPageState extends State<AddUsersPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(254, 245, 245, 245),
       appBar: AppBar(
         title: const Text(
           "Thêm Khách Hàng",
@@ -28,7 +29,7 @@ class _AddUsersPageState extends State<AddUsersPage> {
           icon: const Icon(
             Icons.arrow_back,
             color: Colors.white,
-            ),
+          ),
           onPressed: () {
             context.go('/home');
           },
@@ -43,8 +44,9 @@ class _AddUsersPageState extends State<AddUsersPage> {
             _buildContentTitle(() {
               logger.v("a2.....tam1......");
             }),
-            //  const SizedBox(height: 20),
-            ],
+            const SizedBox(height: 20),
+            _buildContentItem("Nguyen An", "222999333", () {}),
+          ],
         ),
       ),
     );
@@ -56,25 +58,105 @@ class _AddUsersPageState extends State<AddUsersPage> {
         actionClick();
       },
       child: SizedBox(
-         height: 50.0,
+        height: 50.0,
         child: Stack(
           fit: StackFit.expand,
           children: [
             const Center(
               child: Text(
-                "Khách Hàng",style: TextStyle(
-                  color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
+                "Khách Hàng",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
             Positioned(
-            top: 10,
-            right: 20,
-            child: SizedBox(
-                      height: 30.0,
-                      width: 30.0,
-                      child: Image.asset(AppAssets.icAdd) ),
-          )
+              top: 10,
+              right: 20,
+              child: SizedBox(
+                  height: 30.0,
+                  width: 30.0,
+                  child: Image.asset(AppAssets.icAdd)),
+            )
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContentItem(String name, String phone, Function() actionClick) {
+    return GestureDetector(
+      onTap: () {
+        actionClick();
+      },
+      child: SizedBox(
+        height: 60,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(
+                color: Colors.grey.withOpacity(0.5),
+                width: 1.0,
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const SizedBox(width: 10),
+                Transform.scale(
+                  scale: 0.7,
+                  child: Image.asset(AppAssets.icEditUser),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: SizedBox(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            const Text("Tên:  ",
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 10.0,
+                                    fontWeight: FontWeight.w100)),
+                            Text(name,
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            const Text("sdt:  ",
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 10.0,
+                                    fontWeight: FontWeight.w100)),
+                            Text(phone,
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Transform.scale(
+                  scale: 0.8,
+                  child: Image.asset(AppAssets.icNextItem),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
