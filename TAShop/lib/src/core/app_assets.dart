4bcdf0039e558380e89b471f104764f6b07e1c5f
Tam Vu <tam_vu@onskyhealth.com>
2024-08-16 08:48:27 +0700

    *Update code

--- TAShop/lib/src/core/app_assets.dart
+++ TAShop/lib/src/core/app_assets.dart
@@ -1,6 +1,3 @@
 class AppAssets {
   static const String icAddPersons = 'assets/ic_add_persons.png';
-  static const String icEditUser = 'assets/ic_edit_user.png';
-  static const String icAddUser = 'assets/ic_add_user.png';
-  static const String icNextItem = 'assets/ic_next_item.png';
 }
